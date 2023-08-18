b190934d4206bb1d5908d496909271d102563893
Hidetaka Okamoto <kokkoku214@gmail.com>
2023-08-18 23:59:44 +0900

    word break

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import { SiteInfo } from '@/app/libs/microcms'
 
 export function Footer({ siteInfo }: { siteInfo: SiteInfo }) {
   return (
-    <footer className='footer footer-center p-4 bg-base-300 text-base-content'>
+    <footer className='footer footer-center p-4 bg-base-300 text-base-content break-words '>
       <div>
         <p>{siteInfo.description}</p>
         <p>Copyright © 2023 - All right reserved by Hidetaka Okamoto</p>
